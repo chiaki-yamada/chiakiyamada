5dc68b4da3384b1cf320906663fa93ec1356ed6b
chiaki-yamada <110213744+chiaki-yamada@users.noreply.github.com>
2023-03-28 14:48:26 -0700

    updated js

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const send = document.getElementById('send');
 const mask = document.getElementById('mask');
 const modal = document.getElementById('modal');
 
-send.addEventListener('click', () => {
+send.addEventListener('click', (e) => {
+  e.preventDefault();
   mask.classList.remove('hidden');
   modal.classList.remove('hidden');
 });
@@ -13,23 +14,21 @@ mask.addEventListener('click', () => {
 
 
 
-
-
-// const firstName = document.getElementById('name1');
-// const lastName = document.getElementById('name2');
-// const email = document.getElementById('emailaddress');
-// const message = document.getElementById('messageform');
-// send.addEventListener('click', e =>{
-//   if (firstName.value === ""){
-//     alert("Please fill out your first name.")
-//   }else if (lastName.value === ''){
-//     alert("Please fill out your last name.")
-//   }else if (email.value ===''){
-//     alert("Please fill out your email address.")
-//   }else if(message.value === ''){
-//     alert("Please fill out message.")
-//   }
-// });
+const firstName = document.getElementById('name1');
+const lastName = document.getElementById('name2');
+const email = document.getElementById('emailaddress');
+const message = document.getElementById('messageform');
+send.addEventListener('click', e =>{
+  if (firstName.value === ""){
+    alert("Please fill out your first name.")
+  }else if (lastName.value === ''){
+    alert("Please fill out your last name.")
+  }else if (email.value ===''){
+    alert("Please fill out your email address.")
+  }else if(message.value === ''){
+    alert("Please fill out message.")
+  }
+});
 
 
 
