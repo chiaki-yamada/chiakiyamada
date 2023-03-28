264405fb3532bde1baa526211419bae8710bc229
chiaki-yamada <110213744+chiaki-yamada@users.noreply.github.com>
2023-03-28 14:48:56 -0700

    updated js

--- app.js
+++ app.js
@@ -14,21 +14,6 @@ mask.addEventListener('click', () => {
 
 
 
-const firstName = document.getElementById('name1');
-const lastName = document.getElementById('name2');
-const email = document.getElementById('emailaddress');
-const message = document.getElementById('messageform');
-send.addEventListener('click', e =>{
-  if (firstName.value === ""){
-    alert("Please fill out your first name.")
-  }else if (lastName.value === ''){
-    alert("Please fill out your last name.")
-  }else if (email.value ===''){
-    alert("Please fill out your email address.")
-  }else if(message.value === ''){
-    alert("Please fill out message.")
-  }
-});
 
 
 
