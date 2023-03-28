d9ff6cdf1eb89e2cca31781ba73a48703bf5f7c0
chiaki-yamada <110213744+chiaki-yamada@users.noreply.github.com>
2023-03-28 08:16:12 -0700

    Created js

---
+++ app.js
@@ -0,0 +1,5 @@
+const firstName = document.getElementById('name1');
+const lastName = document.getElementById('name2');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+const button = document.getElementById('btn');
