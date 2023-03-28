7e25f717e506ea5f1b8a2b74f8be723d29b29193
chiaki-yamada <110213744+chiaki-yamada@users.noreply.github.com>
2023-03-28 08:31:08 -0700

    Updated js

--- app.js
+++ app.js
@@ -2,4 +2,10 @@ const firstName = document.getElementById('name1');
 const lastName = document.getElementById('name2');
 const email = document.getElementById('email');
 const message = document.getElementById('message');
-const button = document.getElementById('btn');
+const send = document.getElementById('send');
+
+send.addEventListener('click', e =>{
+  if (firstName.value === ''){
+    alert("Please fill out your first name.")
+  }
+});
